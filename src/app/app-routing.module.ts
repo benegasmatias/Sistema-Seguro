a591ba14e887b7cc646846ce0a27e7a803427519
enzo <enzogclcc2@gmail.com>
2019-07-26 00:26:41 -0300

    primera subida

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {SidebarComponent} from './components/navigation/sidebar/sidebar.component'
+import{ClientFormComponent} from './components/clients/client-form/client-form.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:SidebarComponent,
+    pathMatch: 'full'
+  },
+  {
+    path:'clients/add',
+    component:ClientFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/navigation/sidebar/sidebar.component';
+import { ClientFormComponent } from './components/clients/client-form/client-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    ClientFormComponent
   ],
   imports: [
     BrowserModule,
